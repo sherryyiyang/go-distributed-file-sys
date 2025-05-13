60307080016554b9728a3221a1544e5303d2a700
Sherry Yang <sherryyiyang@gmail.com>
2025-05-12 18:01:40 -0700

    encoding

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -5,3 +5,37 @@ import (
 	"io"
 )
 
+type Decoder interface {
+	Decode(io.Reader, *RPC) error
+}
+
+type GOBDecoder struct{}
+
+func (dec GOBDecoder) Decode(r io.Reader, msg *RPC) error {
+	return gob.NewDecoder(r).Decode(msg)
+}
+
+type DefaultDecoder struct{}
+
+func (dec DefaultDecoder) Decode(r io.Reader, msg *RPC) error {
+	peekBuf := make([]byte, 1)
+	if _, err := r.Read(peekBuf); err != nil {
+		return nil
+	}
+
+	stream := peekBuf[0] == IncomingStream
+	if stream {
+		msg.Stream = true
+		return nil
+	}
+
+	buf := make([]byte, 1028)
+	n, err := r.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	msg.Payload = buf[:n]
+
+	return nil
+}
